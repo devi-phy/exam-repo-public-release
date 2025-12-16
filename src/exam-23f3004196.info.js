a71a594a253a988a50d365c86229ee7739132566
devi-phy <23f3004196@ds.study.iitm.ac.in>
2025-12-16 16:53:27 +0530

    Add TDS Bonus Activity configuration file

---
+++ src/exam-23f3004196.info.js
@@ -0,0 +1,7 @@
+export default {
+  title: "TDS Bonus Activity – 23f3004196",
+  start: "2025-12-01T00:00:00+05:30",
+  end: () => "2025-12-31T23:59:59+05:30",
+  allowed: () => true,
+  read: () => true,
+};
